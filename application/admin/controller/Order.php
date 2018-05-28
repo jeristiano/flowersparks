2ddeb902fb78cdd0b89c2b2cc8b42d92ba08cde6
jeremykuang@foxmail.com <jeremykuang@foxmail.com>
2018-05-28 21:03:07 +0800

    由于生病更新缓慢

--- application/admin/controller/Order.php
+++ application/admin/controller/Order.php
@@ -8,85 +8,81 @@ use app\common\controller\Backend;
  * 订单管理
  * @icon fa fa-circle-o
  */
-class Order extends Backend
-{
+class Order extends Backend {
 
-    /**
-     * Order模型对象
-     * @var \app\admin\model\Order
-     */
-    protected $model = null;
+	/**
+	 * Order模型对象
+	 * @var \app\admin\model\Order
+	 */
+	protected $model = null;
 
-    public function _initialize()
-    {
-        parent::_initialize();
-        $this->model = model('Order');
-        $this->view->assign("ifPaidList", $this->model->getIfPaidList());
-    }
+	public function _initialize() {
+		parent::_initialize();
+		$this->model = model('Order');
+		$this->view->assign("ifPaidList", $this->model->getIfPaidList());
+	}
 
-    /**
-     * 默认生成的控制器所继承的父类中有index/add/edit/del/multi五个基础方法、destroy/restore/recyclebin三个回收站方法
-     * 因此在当前控制器中可不用编写增删改查的代码,除非需要自己控制这部分逻辑
-     * 需要将application/admin/library/traits/Backend.php中对应的方法复制到当前控制器,然后进行修改
-     */
+	/**
+	 * 默认生成的控制器所继承的父类中有index/add/edit/del/multi五个基础方法、destroy/restore/recyclebin三个回收站方法
+	 * 因此在当前控制器中可不用编写增删改查的代码,除非需要自己控制这部分逻辑
+	 * 需要将application/admin/library/traits/Backend.php中对应的方法复制到当前控制器,然后进行修改
+	 */
 
+	/**
+	 * 查看
+	 */
+	public function index() {
+		//当前是否为关联查询
+		$this->relationSearch = true;
+		//设置过滤方法
+		$this->request->filter(['strip_tags']);
+		if ($this->request->isAjax()) {
+			//如果发送的来源是Selectpage，则转发到Selectpage
+			if ($this->request->request('keyField')) {
+				return $this->selectpage();
+			}
+			list($where, $sort, $order, $offset, $limit) = $this->buildparams();
+			$total = $this->model
+				->with(['flower', 'user'])
+				->where($where)
+				->order($sort, $order)
+				->count();
 
-    /**
-     * 查看
-     */
-    public function index()
-    {
-        //当前是否为关联查询
-        $this->relationSearch = true;
-        //设置过滤方法
-        $this->request->filter(['strip_tags']);
-        if ($this->request->isAjax()) {
-            //如果发送的来源是Selectpage，则转发到Selectpage
-            if ($this->request->request('keyField')) {
-                return $this->selectpage();
-            }
-            list($where, $sort, $order, $offset, $limit) = $this->buildparams();
-            $total = $this->model
-                ->with(['flower', 'user'])
-                ->where($where)
-                ->order($sort, $order)
-                ->count();
+			$list = $this->model
+				->with(['flower', 'user'])
+				->where($where)
+				->order($sort, $order)
+				->limit($offset, $limit)
+				->select();
 
-            $list = $this->model
-                ->with(['flower', 'user'])
-                ->where($where)
-                ->order($sort, $order)
-                ->limit($offset, $limit)
-                ->select();
+			$list = collection($list)->toArray();
+			$result = array("total" => $total, "rows" => $list);
 
-            $list = collection($list)->toArray();
-            $result = array("total" => $total, "rows" => $list);
+			return json($result);
+		}
+		return $this->view->fetch();
+	}
 
-            return json($result);
-        }
-        return $this->view->fetch();
-    }
+	//添加订单
+	public function add() {
+		if ($this->request->isPost()) {
+			$order_bn = $this->model->makeOrderNo();
+			$params = $this->request->post("row/a");
+			if ($params) {
+				$this->model->order_bn = $order_bn;
+				$params['order_sn'] = $order_bn;
+			}
 
-    public function add()
-    {
-        if ($this->request->isPost()) {
-            $order_bn = $this->model->makeOrderNo();
-            $params = $this->request->post("row/a");
-            if ($params) {
-                $this->model->order_bn = $order_bn;
-                $params['order_sn'] = $order_bn;
-            }
+			$result = $this->model->allowField(true)->save($params);
+			if ($result) {
+				$this->success();
+			} else {
+				$this->error($this->model->getError());
+			}
+		}
 
-            $result = $this->model->allowField(true)->save($params);
-            if($result){
-                $this->success();
-            }else{
-                $this->error($this->model->getError());
-            }
-        }
+		return $this->view->fetch();
 
-        return $this->view->fetch();
-
-    }
+	}
 
 }
