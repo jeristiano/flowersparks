1407c0a20aef6d6ff83878f5318c901d05af9b10
jeremykuang@foxmail.com <jeremykuang@foxmail.com>
2018-06-07 15:31:10 +0800

    订单统计以及缓存

--- application/admin/controller/Dashboard.php
+++ application/admin/controller/Dashboard.php
@@ -9,7 +9,6 @@ use think\Config;
 
 /**
  * 控制台
- *
  * @icon fa fa-dashboard
  * @remark 用于展示当前系统中的统计数据、统计报表及重要实时数据
  */
@@ -22,39 +21,41 @@ class Dashboard extends Backend
      */
     public function index()
     {
-       return Calculate::countUsersTotalAmountThisYear();
+
         $seventtime = \fast\Date::unixtime('day', -7);
         $paylist = $createlist = [];
-        for ($i = 0; $i < 7; $i++)
-        {
+        for ($i = 0; $i < 7; $i++) {
             $day = date("Y-m-d", $seventtime + ($i * 86400));
             $createlist[$day] = mt_rand(20, 200);
             $paylist[$day] = mt_rand(1, mt_rand(1, $createlist[$day]));
         }
-        $today=time();
-        $thisYear=date('Y',$today);
+        $today = time();
+        $thisYear = date('Y', $today);
         $hooks = config('addons.hooks');
         $uploadmode = isset($hooks['upload_config_init']) && $hooks['upload_config_init'] ? implode(',', $hooks['upload_config_init']) : 'local';
         $addonComposerCfg = ROOT_PATH . '/vendor/karsonzhang/fastadmin-addons/composer.json';
         Config::parse($addonComposerCfg, "json", "composer");
         $config = Config::get("composer");
         $addonVersion = isset($config['version']) ? $config['version'] : __('Unknown');
+
+        $calculate = new Calculate();
+        $data = $calculate->preOrderCalculateData();
         $this->view->assign([
-            'totaluser'        => 35200,
-            'totalviews'       => 219390,
-            'totalorder'       => 32143,
-            'totalorderamount' => 174800,
-            'todayuserlogin'   => 321,
-            'todayusersignup'  => 430,
-            'todayorder'       => 2324,
-            'unsettleorder'    => 132,
-            'sevendnu'         => '77%',
-            'sevendau'         => '77%',
-            'paylist'          => $paylist,
-            'createlist'       => $createlist,
-            'addonversion'       => $addonVersion,
-            'uploadmode'       => $uploadmode,
-            'thisyear'=>$thisYear
+            'totaluser' => $data['totaluser'],
+            'totalaverageprice' => $data['totalaverageprice'],
+            'totalorder' => $data['totalorder'],
+            'totalorderamount' => $data['totalorderamount'],
+            'todayuserlogin' => 321,
+            'todayusersignup' => 430,
+            'todayorder' => 2324,
+            'unsettleorder' => 132,
+            'sevendnu' => '77%',
+            'sevendau' => '77%',
+            'paylist' => $paylist,
+            'createlist' => $createlist,
+            'addonversion' => $addonVersion,
+            'uploadmode' => $uploadmode,
+            'thisyear' => $thisYear
         ]);
 
         return $this->view->fetch();
--- application/admin/model/Calculate.php
+++ application/admin/model/Calculate.php
@@ -4,13 +4,57 @@ namespace app\admin\model;
 
 //统计数据服务模型
 use fast\Date;
+use think\Cache;
 
 class Calculate
 {
-    public static function countOrderTotalAmountThisYear()
+    protected $cache = 'thisyearstat';
+
+    public function  preOrderCalculateData()
     {
-        $thisYearUnix = Date::unixtime('year', 0, 'begin');
+        $result = Cache::get($this->cache);
+        if ($result === false) {
+            $data['totaluser'] = $this->countUserAmountThisYear();
+            $data['totalaverageprice'] = $this->countAveragePriceThisYear();
+            $data['totalorder'] = $this->countOrderAmountThisYear();
+            $data['totalorderamount'] = $this->countIncomeAmountThisYear();
+            Cache::set($this->cache, $data, 7200);
+            return $data;
+        } else {
+            return $result;
+        }
+
+    }
+
+    public function countOrderAmountThisYear()
+    {
+        $thisYearUnix = self::thisYearUnix();
         return Order::where('create_time', '>=', $thisYearUnix)->count();
     }
 
+    public function countUserAmountThisYear()
+    {
+        $thisYearUnix = self::thisYearUnix();
+        $users = Order::where('create_time', '>=', $thisYearUnix)->column('user_id');
+        return $userAmount = count(array_unique($users));
+    }
+
+    public function countIncomeAmountThisYear()
+    {
+        $thisYearUnix = self::thisYearUnix();
+        return Order::where('create_time', '>=', $thisYearUnix)->sum('subtotal');
+
+    }
+
+    public function countAveragePriceThisYear()
+    {
+        $thisYearUnix = self::thisYearUnix();
+        return Order::where('create_time', '>=', $thisYearUnix)->avg('price');
+
+    }
+
+    private static function thisYearUnix()
+    {
+        return Date::unixtime('year', 0, 'begin');
+    }
 }
